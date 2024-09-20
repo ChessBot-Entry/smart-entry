e30961b66b6c9236fe89385c96da119d872adc89
ChessBot-Entry <chessbot.entry@gmail.com>
2024-09-21 02:02:03 +0900

    🎨 SmartEntry 객체에 인자 주지 않도록 변경

--- src/Main.ts
+++ src/Main.ts
@@ -1,11 +1,9 @@
 import SmartEntry from "./js/SmartEntry.ts";
 import { waitForElement } from "./js/Inject.ts";
-import './css/Workspace.css';
-import { IEntry } from "../types/entry.js";
 
 (async () => {
     const entryjsFinder = () => {
-        for (let script of document.scripts) {
+        for (let script of Array.from(document.scripts)) {
             if (!script.src) continue
 
             let loc = script.src.split('/')
@@ -25,13 +23,13 @@ import { IEntry } from "../types/entry.js";
         { attributes: false, childList: true, subtree: false })
 
     if ('Entry' in window) {
-        //TODO: global 타입 적용
-        window.SmartEntry = new SmartEntry(window.Entry as IEntry)
+        // TODO: global 타입 적용
+        window.SmartEntry = new SmartEntry()
         return
     }
     
     entryjs.addEventListener('load', () => {
-        if ('Entry' in window) window.SmartEntry = new SmartEntry(window.Entry as IEntry)
+        if ('Entry' in window) window.SmartEntry = new SmartEntry()
         else console.warn("Entry 객체를 찾지 못했습니다")
     })
 })()

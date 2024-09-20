3b6361da61ec7d08d4ce41b8920284d90379d413
ChessBot-Entry <chessbot.entry@gmail.com>
2024-09-21 05:57:49 +0900

    ➕🔧 babel 버전업 + vite에 추가 (decorator 사용)

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 import package_json from './package.json'
+import babel from 'vite-plugin-babel'
 
 manifest.version = package_json.version
 
 export default defineConfig({
-  plugins: [crx({ manifest })],
+  plugins: [crx({ manifest }), babel()],
   build: {
     minify: 'terser',
     terserOptions: {

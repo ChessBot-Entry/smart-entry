310acd8b14bcd00cffc58f0efdfb55e70f3440bb
ChessBot-Entry <chessbot.entry@gmail.com>
2024-09-21 02:02:43 +0900

    🚧 작업 중

--- src/js/SmartEntry.ts
+++ src/js/SmartEntry.ts
@@ -1,9 +1,8 @@
-import { IEntry } from "../../types/entry.js"
+// tsconfig.json에 type 자동으로 불러오게 해뒀는데 적용이 안됨, 왜지?
 
 export default class SmartEntry {
-    EntryObject: IEntry
-    constructor(Entry: IEntry) {
-        this.EntryObject = Entry
-        console.log("test")
+    constructor() {
+        throw Error("Not implemented")
     }
 }
+
